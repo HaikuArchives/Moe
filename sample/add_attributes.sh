6ad8d2b17da3710445220cb46d703c798fe7ba96
Puck Meerburg <puck@puckipedia.nl>
2015-01-18 18:28:52 +0000

    Add attribute adder

---
+++ sample/add_attributes.sh
@@ -0,0 +1,10 @@
+addattr -t mime BEOS:TYPE image/png moe.png
+addattr -t mime BEOS:PREF_APP application/x-vnd.Kamnagi.Moe moe.png
+addattr -t float MOE:Y_RATE 0.287425 moe.png
+addattr -t bool MOE:Y_LOCKED 1 moe.png
+addattr -t float MOE:X_RATE 0.993443 moe.png
+addattr -t bool MOE:X_LOCKED 0 moe.png
+addattr -t float MOE:SIZE 1.000000 moe.png
+addattr -t int MOE:SIDE 0 moe.png
+addattr -f wink1.bin -t "'MSGG'" MOE:ANIME:WINK1 moe.png
+addattr -f wink2.bin -t "'MSGG'" MOE:ANIME:WINK2 moe.png
